0c95ed9ad412094f72aa8ebf218e7dcdf6db93ca
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-24 16:22:48 -0600

    convert server to esm

---
+++ server/index.mjs
@@ -0,0 +1,51 @@
+import { createRequestHandler } from "@remix-run/express"
+import compression from "compression"
+import express from "express"
+import morgan from "morgan"
+import { createRequire } from "node:module"
+import { join } from "node:path"
+
+const require = createRequire(import.meta.url)
+
+const MODE = process.env.NODE_ENV
+const BUILD_DIR = join(process.cwd(), "server/build")
+
+const app = express()
+app.use(compression())
+
+// You may want to be more aggressive with this caching
+app.use(express.static("public", { maxAge: "1h" }))
+
+// Remix fingerprints its assets so we can cache forever
+app.use(express.static("public/build", { immutable: true, maxAge: "1y" }))
+
+app.use(morgan("tiny"))
+app.all(
+  "*",
+  MODE === "production"
+    ? createRequestHandler({ build: require("./build") })
+    : (req, res, next) => {
+        purgeRequireCache()
+        const build = require("./build")
+        return createRequestHandler({ build, mode: MODE })(req, res, next)
+      },
+)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
+})
+
+////////////////////////////////////////////////////////////////////////////////
+function purgeRequireCache() {
+  // purge require cache on requests for "server side HMR" this won't let
+  // you have in-memory objects between requests in development,
+  // alternatively you can set up nodemon/pm2-dev to restart the server on
+  // file changes, we prefer the DX of this though, so we've included it
+  // for you by default
+  for (const key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key]
+    }
+  }
+}
