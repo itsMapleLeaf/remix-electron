aa81d330c12b0f6a022015425b234176750eb3ba
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-24 17:12:31 -0600

    demo electron apis in route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,20 @@
+import { useLoaderData } from "remix"
+import { app } from "~/electron.server"
+
+export function loader() {
+  return {
+    version: app.getVersion(),
+    homePath: app.getPath("home"),
+  }
+}
+
 export default function Index() {
+  const data = useLoaderData()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to Remix (in Electron)</h1>
+      <p>Version: {data.version}</p>
+      <p>Home: {data.homePath}</p>
     </div>
-  );
+  )
 }
