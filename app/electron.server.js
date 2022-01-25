b13ce53ed873985ee793ca607c85aaf9e1ace295
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-24 18:19:32 -0600

    add comment explaining electron.server.js

--- app/electron.server.js
+++ app/electron.server.js
@@ -1,1 +1,5 @@
+// @ts-nocheck
+// attempting `export * from "electron"` in a TS file results in an error
+// due to TS module weirdness
+// so instead, we have this untyped js file w/ a typed .d.ts file
 export * from "electron"
