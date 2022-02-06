3f782e3d5b4a8b9c294d64421afb8302e831da73
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-05 21:12:52 -0600

    serve JS correctly + some cleanup

--- desktop/register-remix-protocol.js
+++ desktop/register-remix-protocol.js
@@ -2,6 +2,9 @@
 const { app, protocol } = require("electron")
 const { createRequestHandler } = require("@remix-run/server-runtime")
 const { Request, Response, Headers } = require("@remix-run/node")
+const { relative, join } = require("path")
+const { stat, readFile } = require("fs/promises")
+const mime = require("mime")
 
 // need to declare these globally, otherwise remix errors
 // @ts-expect-error
@@ -11,40 +14,96 @@ globalThis.Response = Response
 // @ts-expect-error
 globalThis.Headers = Headers
 
+const publicFolder = join(__dirname, "../public")
+const buildFolder = join(publicFolder, "build")
+const mode = app.isPackaged ? "production" : process.env.NODE_ENV
+
 exports.registerRemixProtocol = function registerRemixProtocol() {
-  /** @type {any} */
-  const build = require("./build")
-
-  const handleRequest = createRequestHandler(
-    build,
-    {},
-    app.isPackaged ? "production" : process.env.NODE_ENV,
-  )
-
   protocol.registerStringProtocol("remix", async (request, callback) => {
     try {
-      const remixRequest = new Request(request.url, {
-        method: request.method,
-        headers: request.headers,
-      })
+      const assetResponse = await tryServeFile(request)
+      if (assetResponse) {
+        callback(assetResponse)
+        return
+      }
 
-      remixRequest.headers.set("referrer", request.referrer)
-
-      // TODO: add uploadData to request as FormData
-      // @ts-expect-error
-      const response = await handleRequest(remixRequest)
-
-      callback({
-        data: await response.text(),
-        headers: Object.fromEntries(response.headers),
-        statusCode: response.status,
-        url: response.url,
-      })
+      callback(await serveRemixResponse(request))
     } catch (error) {
       callback({
         statusCode: 500,
         data: `<pre>${error.stack || error.message || String(error)}</pre>`,
       })
+      console.warn(error)
     }
   })
+}
+
+/**
+ * @param {Electron.ProtocolRequest} request
+ */
+async function tryServeFile(request) {
+  const url = new URL(request.url)
+  const filePath = join(publicFolder, url.pathname)
+  if (!(await isFile(filePath))) return
+
+  return {
+    data: await readFile(filePath, "utf-8"),
+    mimeType: mime.getType(filePath),
+  }
+}
+
+/**
+ * @param {Electron.ProtocolRequest} request
+ */
+async function serveRemixResponse(request) {
+  const remixRequest = new Request(request.url, {
+    method: request.method,
+    headers: request.headers,
+  })
+
+  remixRequest.headers.set("referrer", request.referrer)
+
+  // TODO: add uploadData to request as FormData
+
+  if (mode === "development") {
+    purgeRequireCache()
+  }
+
+  /** @type {any} */
+  const build = require("./build")
+  const handleRequest = createRequestHandler(build, {}, mode)
+  // @ts-expect-error
+  const response = await handleRequest(remixRequest)
+
+  return {
+    data: await response.text(),
+    headers: {
+      ...Object.fromEntries(response.headers),
+      "content-security-policy": [
+        "default-src 'self'",
+        "script-src 'self' 'unsafe-inline'",
+        "style-src 'self' 'unsafe-inline'",
+        "connect-src ws:",
+        "base-uri 'self'",
+      ].join("; "),
+    },
+    statusCode: response.status,
+    mimeType: response.headers.get("content-type"),
+  }
+}
+
+/**
+ * @param {import("fs").PathLike} path
+ */
+async function isFile(path) {
+  const stats = await stat(path).catch(() => undefined)
+  return stats?.isFile() ?? false
+}
+
+function purgeRequireCache() {
+  for (const key in require.cache) {
+    if (key.startsWith(buildFolder)) {
+      delete require.cache[key]
+    }
+  }
 }
