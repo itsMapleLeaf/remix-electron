021cbf0c6f40df38ab27150268c788c6244b8314
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-05 21:25:58 -0600

    open devtools in dev

--- desktop/window.js
+++ desktop/window.js
@@ -7,5 +7,8 @@ let win
 exports.createWindow = async function createWindow() {
   win = new BrowserWindow({ show: false })
   await win.loadURL("remix://app/")
+  if (process.env.NODE_ENV === "development") {
+    win.webContents.openDevTools()
+  }
   win.show()
 }
