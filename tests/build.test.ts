bcbe1765e66cd8324c96e93cf71b55cdb83af810
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-07 01:36:11 -0600

    add build test

---
+++ tests/build.test.ts
@@ -0,0 +1,41 @@
+import { execa } from "execa"
+import { join } from "node:path"
+import type { ElectronApplication, Page } from "playwright"
+import { _electron as electron } from "playwright"
+import { afterAll, beforeAll, expect, test } from "vitest"
+
+const appFolder = join(__dirname, "../template")
+
+let electronApp: ElectronApplication
+let window: Page
+
+beforeAll(async () => {
+  await execa("pnpm", ["build", "--", "--dir"], {
+    cwd: appFolder,
+    stdio: "inherit",
+  })
+
+  electronApp = await electron.launch({
+    cwd: appFolder,
+    args: ["."],
+    executablePath: join(
+      appFolder,
+      "dist/linux-unpacked/remix-electron-template",
+    ),
+
+    // this fixes a failure to launch on linux
+    env: {
+      DISPLAY: process.env.DISPLAY!,
+    },
+  })
+
+  window = await electronApp.firstWindow()
+}, 1000 * 60)
+
+afterAll(async () => {
+  await electronApp.close()
+})
+
+test("packaged build", async () => {
+  expect(await window.locator("h1").textContent()).toBe("Welcome to Remix")
+})
