e8f9425743c4b886e6316dcd3d855b9b80728f3c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-18 16:07:44 -0600

    update template to compile electron entry with remix

---
+++ template/desktop/main.ts
@@ -0,0 +1,26 @@
+// @ts-check
+import * as serverBuild from "@remix-run/dev/server-build"
+import { app, BrowserWindow, dialog } from "electron"
+import { initRemix } from "remix-electron"
+
+let win: BrowserWindow | undefined
+
+async function createWindow(url: string) {
+  win = new BrowserWindow({ show: false })
+  await win.loadURL(url)
+  win.show()
+}
+
+app.on("ready", async () => {
+  try {
+    const url = await initRemix({ serverBuild })
+    await createWindow(url)
+  } catch (error) {
+    dialog.showErrorBox("Error", getErrorStack(error))
+    console.error(error)
+  }
+})
+
+function getErrorStack(error: unknown) {
+  return error instanceof Error ? error.stack || error.message : String(error)
+}
--- template/remix.config.js
+++ template/remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
+  server: "desktop/main.ts",
   serverBuildPath: "desktop/build/index.js",
   devServerPort: 8002,
   ignoredRouteFiles: [".*"],
