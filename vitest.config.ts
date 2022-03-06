e71c393ed3c9772704ff4e96aff06483d18436f5
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-05 20:42:10 -0600

    exclude build outside of linux via test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite"
+import { configDefaults, defineConfig } from "vitest/config"
+
+const exclude = [...configDefaults.exclude!]
+
+if (process.platform !== "linux") {
+  console.info("Skipping build test on non-linux platform")
+  exclude.push("tests/build.test.ts")
+}
 
 export default defineConfig({
   test: {
     threads: false,
+    include: ["{src,tests}/**/*.test.{ts,tsx}"],
+    exclude,
   },
 })
