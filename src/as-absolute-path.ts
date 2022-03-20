24790fd02047c3c6e7416d547975da4b8208a9ad
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-20 15:08:53 -0500

    v2 restructure

--- src/as-absolute-path.ts
+++
@@ -1,5 +0,0 @@
-import { isAbsolute, join } from "node:path"
-
-export function asAbsolutePath(filePath: string): string {
-  return isAbsolute(filePath) ? filePath : join(process.cwd(), filePath)
-}
