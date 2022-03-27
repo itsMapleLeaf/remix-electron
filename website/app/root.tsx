b9d7d88ecb79fbc30a59f61d39bbdbdea412d200
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-27 18:38:59 -0500

    set up tailwind

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -6,7 +6,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react"
-import type { MetaFunction } from "@remix-run/server-runtime"
+import type { LinksFunction, MetaFunction } from "@remix-run/server-runtime"
+import tailwindCss from "./tailwind.css"
 
 export const meta: MetaFunction = () => ({
   // eslint-disable-next-line unicorn/text-encoding-identifier-case
@@ -15,9 +16,13 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 })
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: tailwindCss },
+]
+
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-900 text-gray-50">
       <head>
         <Meta />
         <Links />
---
+++ website/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["app/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
