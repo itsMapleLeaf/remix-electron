b14cd232c2f813cbe00055a7c7c4cbb12577b4c6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-27 18:22:46 -0500

    init website remix project

---
+++ website/app/root.tsx
@@ -0,0 +1,33 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react"
+import type { MetaFunction } from "@remix-run/server-runtime"
+
+export const meta: MetaFunction = () => ({
+  // eslint-disable-next-line unicorn/text-encoding-identifier-case
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+})
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
