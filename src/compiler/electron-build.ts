49b009dda644a447cb2ae3b98d575e1391dbef3b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-27 22:25:57 -0500

    update electron build config

--- src/compiler/electron-build.ts
+++ src/compiler/electron-build.ts
@@ -1,25 +1,28 @@
+import { getAppDependencies } from "@remix-run/dev/compiler/dependencies"
 import type * as esbuild from "esbuild"
 import { builtinModules as nodeBuiltins } from "node:module"
 import type { CompilerMode } from "./compiler-mode"
 import type { RemixElectronConfig } from "./config"
 
-const electronOptions = (mode: CompilerMode): esbuild.BuildOptions => ({
-  bundle: true,
-  format: "cjs",
-  platform: "node",
-  external: [...nodeBuiltins, "electron", "remix-electron"],
-  logLevel: "silent",
-  treeShaking: true,
-  minify: mode === "production",
-  sourcemap: mode === "development" ? "external" : false,
-})
-
 export function getElectronBuildOptions(
   remixElectronConfig: RemixElectronConfig,
   mode: CompilerMode,
 ): esbuild.BuildOptions {
+  const dependencies = getAppDependencies(remixElectronConfig)
   return {
-    ...electronOptions(mode),
+    bundle: true,
+    format: "cjs",
+    platform: "node",
+    external: [
+      ...nodeBuiltins,
+      ...nodeBuiltins.map((name) => `node:${name}`),
+      ...Object.keys(dependencies),
+      "electron", // sometimes electron is included as a dev dependency, and won't get returned from getAppDependencies
+    ],
+    logLevel: "silent",
+    treeShaking: true,
+    minify: mode === "production",
+    sourcemap: mode === "development" ? "external" : false,
     entryPoints: [remixElectronConfig.electronEntryFile],
     outfile: remixElectronConfig.electronBuildFile,
   }
@@ -30,7 +33,7 @@ export function getPreloadBuildOptions(
   mode: CompilerMode,
 ): esbuild.BuildOptions {
   return {
-    ...electronOptions(mode),
+    ...getElectronBuildOptions(remixElectronConfig, mode),
     entryPoints: [remixElectronConfig.preloadEntryFile],
     outfile: remixElectronConfig.preloadBuildFile,
   }
