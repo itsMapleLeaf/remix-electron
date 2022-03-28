0133bae3c2fb9472cffca8a2b1c91cc0615f60f7
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-03-27 23:11:39 -0500

    clean before build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,17 @@
+import { rm } from "node:fs/promises"
+import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
 import * as tsup from "tsup"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const options: tsup.Options = {
   target: "node16",
   platform: "node",
   sourcemap: true,
 }
+
+await rm(join(__dirname, "../dist"), { recursive: true, force: true })
 
 await Promise.all([
   tsup.build({
