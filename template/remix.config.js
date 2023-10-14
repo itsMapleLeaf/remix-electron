e408a29ded20908955111ad457ce21b37e4d876e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-10-14 16:29:41 -0500

    remove future flags

--- template/remix.config.js
+++ template/remix.config.js
@@ -3,11 +3,4 @@
  */
 module.exports = {
   serverModuleFormat: "cjs",
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    unstable_dev: true,
-  },
 }
