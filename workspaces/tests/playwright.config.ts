562de48a928aa9674bcb9b99c5f5c61224e23d66
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-08-30 12:42:51 -0500

    use prettier until biome has markdown support
    ref https://github.com/biomejs/biome/issues/3718

--- workspaces/tests/playwright.config.ts
+++ workspaces/tests/playwright.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "@playwright/test"
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
+/** Read environment variables from file. https://github.com/motdotla/dotenv */
 // require('dotenv').config();
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+/** See https://playwright.dev/docs/test-configuration. */
 export default defineConfig({
 	testDir: "./tests",
 	/* Run tests in files in parallel */
